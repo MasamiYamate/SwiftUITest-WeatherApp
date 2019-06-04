9d54e36e654f0c1d0cac92697e642038f6d03e3f
MasamiYamate <m.yamate@mail.yamatte.net>
2019-06-04 10:58:24 +0900

    Initial Commit

---
+++ SwiftUITest-WeatherAppTests/SwiftUITest_WeatherAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUITest_WeatherAppTests.swift
+//  SwiftUITest-WeatherAppTests
+//
+//  Created by MasamiYamate on 2019/06/04.
+//  Copyright © 2019 net.yamatte. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUITest_WeatherApp
+
+class SwiftUITest_WeatherAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
